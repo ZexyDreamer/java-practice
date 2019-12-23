9fe52b5562c3faabfe372f1956e914934ba704fe
zexydreamer <kedr2800@icloud.com>
2019-12-24 02:37:54 +0500

    add new interface to handle message

--- service/src/main/java/Interfaces/IHandler.java
+++ service/src/main/java/Interfaces/IHandler.java
@@ -1,4 +1,10 @@
 package Interfaces;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+
 public interface IHandler {
+    String handle(ArrayList<KeyboardRow> keyboard, ReplyKeyboardMarkup keyboardMarkup, KeyboardRow firstKeyboardRow, KeyboardRow secondKeyboardRow);
 }
